7c644e05c1a14aa456f71ac60fe036468458935d
Ayush Kant <www.ayushkant@gmail.com>
2025-05-29 07:04:29 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+//->primitive datatypes
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value - khali hai, undefined nahi hai - 0 ni use krte kyuki for eg 0 temp ek value hai
+// undefined => define ni kiye ho variable ko bas declare kiye ho
+// symbol => unique - in react to state any component as unique from other component
+
+//->non primitive or derived datatypes
+// object
+
+console.log(typeof "ayush"); // string
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,5 @@
 console.log("ayush hello world")
+const pi = 3.14
+var city = 'patna'
+let name = "ayush"
+console.table([pi,city, name])
